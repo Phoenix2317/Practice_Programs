0c90c295e0897226e2c113c0b2815d19cd66fe37
Phoenix2317 <ancient.spirit2317@gmail.com>
2025-05-03 15:57:47 -0400

    Using division

---
+++ Four_Sisters_and_Duckbear/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Number of avaible chocolate eggs: ");
+int eggs = Convert.ToInt32(Console.ReadLine());
+int sisters = 4;
+Console.WriteLine("Number of eggs the sisters each get: " + (eggs / sisters));
+Console.WriteLine("Number of eggs the duckbear gets: " + (eggs % sisters));
+if(eggs % sisters > eggs / sisters)
+{
+    Console.WriteLine("The duckBear gets more eggs then the sisters");
+}
+else
+{
+    Console.WriteLine("The sisters get more eggs then the duck bear");
+}
---
+++ Testing_Values/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+int a = 5;
+int b = 2;
+int result = a / b;
+Console.WriteLine(result);
