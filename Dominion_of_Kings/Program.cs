6ad9394da7cd3a8e66c24232bbdd806ac2f09549
Phoenix2317 <ancient.spirit2317@gmail.com>
2025-05-03 16:08:35 -0400

    Using incremental expressions

---
+++ Dominion_of_Kings/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("How many provinces do you own? ");
+int province = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("How many duchies do you own? ");
+int duchies = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("How many estates do you own? ");
+int estates = Convert.ToInt32(Console.ReadLine());
+int score = 0;
+
+for(int i = 0; i < province; i++)
+{
+    score += 6;
+}
+
+for(int i = 0; i < duchies; i++)
+{
+    score += 3;
+}
+
+for(int i = 0; i < estates; i++)
+{
+    score++;
+}
+
+Console.WriteLine("The total value of your kingdom is: " + score);
