55a224501c262b5b11c919d0310e57f6158c0e4c
Phoenix2317 <ancient.spirit2317@gmail.com>
2025-05-03 15:36:50 -0400

    Learned math processing in C#

---
+++ Triangle_Farmer/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter the the triangle base size: ");
+float bottom = Convert.ToSingle(Console.ReadLine());
+Console.WriteLine("Enter the height of the triangle: ");
+float height = Convert.ToSingle(Console.ReadLine());
+float area = (bottom * height) / 2;
+Console.WriteLine("The area of the triangle is: " + area);
