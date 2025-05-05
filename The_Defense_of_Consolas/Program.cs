509010227b38d65207431e88e0035a3aadd7383e
Phoenix2317 <ancient.spirit2317@gmail.com>
2025-05-05 17:22:56 -0400

    Messing with the Console class and string interpolation

---
+++ The_Defense_of_Consolas/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Title = "Defense of Consolas";
+Console.Write("Target Row? ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.Write("Target Column? ");
+int column = Convert.ToInt32(Console.ReadLine());
+int up = row + 1;
+int down = row - 1;
+int left = column - 1; 
+int right = column + 1;
+Console.WriteLine("Deploy to: ");
+//(row, column)
+Console.ForegroundColor = ConsoleColor.Magenta;
+Console.WriteLine($"({row}, {left})");
+Console.ForegroundColor = ConsoleColor.DarkMagenta;
+Console.WriteLine($"({down}, {column})");
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.WriteLine($"({row}, {right})");
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine($"({up}, {column})");
+Console.ForegroundColor = ConsoleColor.White;
+Console.Beep();
