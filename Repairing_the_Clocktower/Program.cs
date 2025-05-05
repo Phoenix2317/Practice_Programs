e638d784a8f881b2c8251d626144d3e6eecbeb4d
Phoenix2317 <ancient.spirit2317@gmail.com>
2025-05-05 17:43:40 -0400

    Using if else statements

---
+++ Repairing_the_Clocktower/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Enter a number for the clock: ");
+int input = Convert.ToInt32(Console.ReadLine());
+
+if (input % 2 == 0)
+{
+
+    Console.WriteLine("Tick!");
+
+} else
+{
+    Console.WriteLine("Tock!");
+}
