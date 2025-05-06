33ed1fbb7abf40813b6c9e4cb392337a8f6deb19
Phoenix2317 <ancient.spirit2317@gmail.com>
2025-05-06 17:56:57 -0400

    Relation statements

---
+++ Watchtower/Program.cs
@@ -0,0 +1,57 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("What is the x direction of the enemy? ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("What is the y direction of the enemy? ");
+int y = Convert.ToInt32(Console.ReadLine());
+//West
+if (x < 0)
+{
+
+    if (y < 0)
+    {
+        Console.WriteLine("Enemy to the Southwest!");
+    }
+    else if (y > 0)
+    {
+        Console.WriteLine("Enemy to the Northwest!");
+    }
+    else if (y == 0)
+    {
+        Console.WriteLine("Enemy to the West!");
+    }
+}
+//east
+else if (x > 0)
+{
+
+    if (y < 0)
+    {
+        Console.WriteLine("Enemy to the Southeast!");
+    }
+    else if (y > 0)
+    {
+        Console.WriteLine("Enemy to the Northeast!");
+    }
+    else if (y == 0)
+    {
+        Console.WriteLine("Enemy to the East!");
+    }
+} 
+//North\South
+else
+{
+
+    if (y < 0)
+    {
+        Console.WriteLine("Enemy to the South!");
+    }
+    else if (y > 0)
+    {
+        Console.WriteLine("Enemy to the North!");
+    }
+    else if (y == 0)
+    {
+        Console.WriteLine("An enemy is here!");
+    }
+
+}
