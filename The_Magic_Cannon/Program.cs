54de8bf58d6d50cc4a19e783e35f943a17575f34
Phoenix2317 <ancient.spirit2317@gmail.com>
2025-05-07 19:41:41 -0400

    Using a for loop to do cannon blasts.

---
+++ The_Magic_Cannon/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Let's start cranking!");
+
+for (int i = 0; i < 100; i++)
+{
+    if ((i + 1) % 3 == 0 && (i + 1) % 5 == 0)
+    {
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine($"{(i + 1)}:  Omega blast!");
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+    else if ((i + 1) % 3 == 0)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"{(i + 1)}:  Fire blast!");
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+    else if ((i + 1) % 5 == 0)
+    {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine($"{(i + 1)}:  Electric Blast!");
+        Console.ForegroundColor = ConsoleColor.White;
+    } else
+    {
+        Console.WriteLine($"{(i + 1)}:  Normal strike");
+    }
+}
