5de4432d571e4f8f219223bcf55052fc3756caa4
Phoenix2317 <ancient.spirit2317@gmail.com>
2025-05-07 20:05:24 -0400

    Using arrays and the range collector

---
+++ The_Replicator_of_D'to/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[] initial = new int[5];
+
+for (int i = 0; i < 5; i++)
+{
+    Console.Write("Enter a value: ");
+    initial[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine("Replicating...");
+
+//copies the entire array. Can do [0..2] to copy the values at 0 and 1 but not 2
+int[] copy = initial[0..];
+
+Console.WriteLine("Done! Displaying now.");
+
+for (int i = 0; i < copy.Length; i++)
+{
+    Console.WriteLine(copy[i]);
+}
