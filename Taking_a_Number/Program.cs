6789b7d6829f74fc821a621c6049bb5aa00c6600
Phoenix2317 <ancient.spirit2317@gmail.com>
2025-05-09 16:11:47 -0400

    Taking a number functions.

---
+++ Taking_a_Number/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+/// <summary>
+/// Displays the given text on the console and returns the entered number
+/// </summary>
+int AskForNumber(string text)
+{
+
+    Console.WriteLine(text);
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+
+}
+
+int AskForNumberInRange(string text, int min, int max)
+{
+    while (true)
+    {
+
+        Console.WriteLine(text);
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        if(num >= min && num <= max)
+        {
+            return num;
+        } else
+        {
+            Console.WriteLine("That value is out of range.");
+        }
+
+    }
+    
+
+}
