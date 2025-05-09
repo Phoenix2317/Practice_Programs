e741e66a09bdbe612f715dfb14d4e03a1497ead0
Phoenix2317 <ancient.spirit2317@gmail.com>
2025-05-09 16:33:12 -0400

    Testing Recursion calls

---
+++ CountDown/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+countDown(10);
+
+///<summary>
+///Counts down from a given int to 1 with each call
+/// </summary>
+void countDown(int num)
+{
+    if (num == 1) Console.WriteLine(num);
+    else
+    {
+        Console.WriteLine(num);
+        countDown(num - 1);
+    }
+
+}
