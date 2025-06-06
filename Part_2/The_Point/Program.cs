11018e793bbfa5691fc4aa62997c80c1bda3d5ad
Phoenix2317 <ancient.spirit2317@gmail.com>
2025-06-05 20:25:56 -0400

    Added a color class to display vallues and added comments to easier tell what which part does

--- Part_2/The_Point/Program.cs
+++ Part_2/The_Point/Program.cs
@@ -1,4 +1,6 @@
-﻿internal class Program
+﻿using System.Xml;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
@@ -6,6 +8,12 @@
         Point Position2 = new Point(-4, 0);
         Console.WriteLine($"The first point is located at:  ({Position1.x}, {Position1.y})");
         Console.WriteLine($"The second point is located at: ({Position2.x}, {Position2.y})");
+
+        Color Pallet = new Color(128, 158, 230);
+        Color Basic = Color.Yellow();
+
+        Console.WriteLine($"The values for the starting pallet are: (Red: {Pallet.r}, Green: {Pallet.g}, Blue: {Pallet.b}) ");
+        Console.WriteLine($"The values for the basic Yellow are:    (Red: {Basic.r}, Green: {Basic.g}, Blue: {Basic.b})");
     }
 
     class Point
@@ -13,18 +21,102 @@
         public int x { get; private set; }
         public int y { get; private set; }
 
-        public Point(int x, int y) 
+        public Point(int x, int y) //Lets the User set a point
         { 
             this.x = x;
             this.y = y;
         }
 
-        public Point() 
+        public Point() //puts a point at the origin
         {
             this.x = 0;
             this.y = 0;
         }
 
 
+    }
+
+    class Color
+    {
+        public int r {  get; private set; }
+        public int g { get; private set; }
+        public int b { get; private set; }
+
+        public static Color White()
+        {
+            Color color = new Color(255, 255, 255);
+            return color;
+        }
+
+        public static Color Black()
+        {
+            return new Color();
+        }
+
+        public static Color Red()
+        {
+            return new Color(255, 0, 0);
+        }
+
+        public static Color Orange()
+        {
+            return new Color(255, 165, 0);
+        }
+
+        public static Color Yellow()
+        {
+            return new Color(255, 255, 0);
+        }
+
+        public static Color Green()
+        {
+            return new Color(0, 128, 0);
+        }
+
+        public static Color Blue()
+        {
+            return new Color(0, 0, 255);
+        }
+
+        public static Color Purple()
+        {
+            return new Color(128, 0, 128);
+        }
+
+        public Color(int r, int g, int b) //Sets the color value to the desired notation, or to 0 if its out of bounds
+        {
+            if(r >= 0 && r <= 255)
+            {
+                this.r = r;
+            } else
+            {
+                this.r = 0;
+            }
+
+            if (g >= 0 && g <= 255)
+            {
+                this.g = g;
+            } else
+            {
+                this.g = 0;
+            }
+
+            if (b >= 0 && b <= 255)
+            {
+                this.b = b;
+            } else
+            {
+                this.b = 0;
+            }
+
+
+        }
+
+        public Color() //defaults all colors to 0
+        {
+            this.r = 0;
+            this.g = 0;
+            this.b = 0;
+        }
     }
 }
