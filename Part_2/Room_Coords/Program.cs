0b3a0f77cf323995aa5da4dfd131a985bc1ff3a5
Phoenix2317 <kb848020@ohio.edu>
2026-06-15 17:11:37 -0400

    Worked on adding coords as structs

---
+++ Part_2/Room_Coords/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+public struct Coordinate
+{
+    public int X { get; }
+    public int Y { get; }
+
+    public Coordinate(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public static bool AreAdjacent(Coordinate a, Coordinate b)
+    {
+        int rowChange = Math.Abs(a.X - b.X);
+        int colChange = Math.Abs(a.Y - b.Y);
+        return ((rowChange <= 1 && colChange == 0) || (rowChange == 0 && colChange <= 1));
+    }
+}
