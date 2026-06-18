dcc9d4d1c527a66cd878766a093a39cfe2a429d6
Phoenix2317 <kb848020@ohio.edu>
2026-06-18 17:50:09 -0400

    Added room coords

--- Part_2/Room_Coords/Program.cs
+++ Part_2/Room_Coords/Program.cs
@@ -1,6 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
+Coordinate a = new Coordinate(4, 5);
+Coordinate b = new Coordinate(4, 6);
+Coordinate c = new Coordinate(3, 6);
+
+Console.WriteLine($"Are {a} and {b} adjacent? {Coordinate.AreAdjacent(a, b)}");
+Console.WriteLine($"Are {a} and {c} adjacent? {Coordinate.AreAdjacent(a, c)}");
+Console.WriteLine($"Are {b} and {c} adjacent? {Coordinate.AreAdjacent(b, c)}");
+
 public struct Coordinate
 {
     public int X { get; }
