5e261438d9c132f26c633239c49172dd81bce2a8
Phoenix2317 <kb848020@ohio.edu>
2026-08-08 10:43:56 -0400

    Learning how to use namespace and about conflicts between namespaces

---
+++ Part_3/The_Feud/Program.cs
@@ -0,0 +1,33 @@
+﻿
+using IField;
+using McDriod;
+
+namespace Farm
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cow cow = new Cow();
+            McDriod.Pig McPig = new McDriod.Pig();
+
+            Sheep sheep = new Sheep();
+            IField.Pig IPig = new IField.Pig();
+            
+        }
+    }
+}
+
+
+namespace IField
+{
+    public class Sheep { }
+    public class Pig { }
+}
+
+namespace McDriod
+{
+    public class Cow { }
+    public class Pig { }
+}
