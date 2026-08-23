bf3ea461a0cec1de2db28aa9f75872a47b613545
Phoenix2317 <ancient.spirit2317@gmail.com>
2026-08-23 16:51:44 -0400

    Learning about Trysparse and out parameters

---
+++ Part_3/Safer_number_Crunching/Program.cs
@@ -0,0 +1,42 @@
+﻿
+while (true)
+{
+    Console.Write("Please enter an Int: ");
+    string? input = Console.ReadLine();
+    if (int.TryParse(input, out int value))
+    {
+        Console.WriteLine($"You entered: {value}");
+        break;
+    }
+    else
+    {
+        Console.WriteLine("That is not a number!");
+    }
+}
+while (true)
+{
+    Console.Write("Enter a decimal: ");
+    string? input = Console.ReadLine();
+    if(double.TryParse(input, out double value))
+    {
+        Console.WriteLine($"You entered: {value}");
+        break;
+    } else
+    {
+        Console.WriteLine("That is not valid!");
+    }
+}
+while (true)
+{
+    Console.Write("True or False: ");
+    string? input = Console.ReadLine();
+    if(bool.TryParse(input, out bool value))
+    {
+        string result = value ? "true" : "false";
+        Console.WriteLine($"That is {result}");
+        break;
+    } else
+    {
+        Console.WriteLine("That is not valid!");
+    }
+}
