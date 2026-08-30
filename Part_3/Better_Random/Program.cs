f03808cb19fa87652fc30899fabaccd58af0c405
Phoenix2317 <ancient.spirit2317@gmail.com>
2026-08-30 16:05:19 -0400

    Working with Random and Extensions

---
+++ Part_3/Better_Random/Program.cs
@@ -0,0 +1,54 @@
+﻿
+Random ran = new Random();
+
+Console.WriteLine(ran.NextDouble(100));
+Console.WriteLine(ran.NextString("Yip", "Yap", "Yop"));
+Console.WriteLine(ran.CoinFlip());
+Console.WriteLine(ran.CoinFlip(0.75));
+
+public static class RandomExtensions
+{
+    /**
+     * Picks a number from 0 to maxVal
+     *
+     */
+    public static double NextDouble(this Random r, double maxVal)
+    {
+        double result = 0;
+
+        result = r.NextDouble();
+
+        result = maxVal * result;
+
+        return result;
+    }
+
+
+    /**
+     * Picks a random string that is passed to it as an array
+     */
+    public static string NextString(this Random r, params string[] wordsLs)
+    {
+        int choice = r.Next(wordsLs.Length);
+        
+        return wordsLs[choice];
+    }
+    /**
+     * Flips a coin, allowing you to change the chance of heads appearing
+     */
+    public static bool CoinFlip(this Random r, double chanceH = 0.5)
+    {
+
+        double coin = r.NextDouble();
+
+        if (coin < chanceH)
+        {
+            return false;
+
+        } else
+        {
+            return true;
+        }
+    }
+
+}
