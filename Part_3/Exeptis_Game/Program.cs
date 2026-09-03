55f102c548081d7e995a005f0e64c27b8827b96c
Phoenix2317 <kb848020@ohio.edu>
2026-09-03 13:24:23 -0400

    Messed around with exceptions and making my own.

---
+++ Part_3/Exeptis_Game/Program.cs
@@ -0,0 +1,46 @@
+﻿
+Random oatmeal = new Random();
+
+int bad = oatmeal.Next(10);
+
+List<int> chosen = new List<int>();
+
+while (true)
+{
+    Console.WriteLine("Pick a number from 0-9:");
+    int guess = int.Parse(Console.ReadLine());
+
+    try
+    {
+        if (guess != bad && !chosen.Contains(guess))
+        {
+            Console.WriteLine("Yay! That was a chocolate cookie!");
+            chosen.Add(guess);
+        }
+        else if (chosen.Contains(guess))
+        {
+            Console.WriteLine("That number has already been taken. Try again.");
+
+        }
+        else
+        {
+            throw new CookieException("Yuck! You got the oatmeal raisn cookie! You lose!");
+        }
+    } catch (CookieException e)
+    {
+        Console.WriteLine(e.Message);
+        break;
+        
+    } finally
+    {
+        chosen.Clear();
+        
+    }
+
+}
+
+public class  CookieException : Exception
+{
+    public CookieException() { }
+    public CookieException(string message) : base(message) { }
+}
