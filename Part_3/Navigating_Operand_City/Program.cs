648d591c3831a1aa70938856c02fa0bd4db6c646
Phoenix2317 <kb848020@ohio.edu>
2026-09-24 12:33:47 -0400

    Added a conversion operator to implicitly convert from direction to Offset

--- Part_3/Navigating_Operand_City/Program.cs
+++ Part_3/Navigating_Operand_City/Program.cs
@@ -50,5 +50,17 @@ public record BlockCoordinate(int Row, int Column)
 
     
 }
-public record BlockOffset(int RowOffset, int ColumnOffset);
+public record BlockOffset(int RowOffset, int ColumnOffset)
+{
+
+    public static implicit operator BlockOffset(Direction dir) => dir switch
+    {
+        Direction.North => new BlockOffset(-1, 0),
+        Direction.East => new BlockOffset(0, 1),
+        Direction.South => new BlockOffset(1, 0),
+        Direction.West => new BlockOffset(0, -1),
+        _ => throw new ArgumentOutOfRangeException(nameof(dir), dir, null)
+    };
+
+}
 public enum Direction { North, East, South, West }
