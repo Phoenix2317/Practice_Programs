a0692c19c84fb9c0bada90c5bf0e7afd2bf79a36
Phoenix2317 <kb848020@ohio.edu>
2026-09-24 12:21:04 -0400

    Messed around with overloading operators.

---
+++ Part_3/Navigating_Operand_City/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+BlockCoordinate startLoc = new BlockCoordinate(0, 0);
+BlockOffset offset = new BlockOffset(1, 2);
+Direction direction = Direction.North;
+
+Console.WriteLine($"Welcome to operand city! Your current location is: {startLoc.Row}, {startLoc.Column}");
+Console.WriteLine("Let's head south first!");
+
+startLoc = startLoc + Direction.South;
+
+Console.WriteLine($"Now we are at: {startLoc.Row}, {startLoc.Column}");
+Console.WriteLine("Now to move Southeast.");
+
+startLoc = startLoc + new BlockOffset(4, 9);
+
+Console.WriteLine($"Now we are at: {startLoc.Row}, {startLoc.Column}");
+Console.WriteLine("Hmm, we seem to have moved to far east. Lets go back a bit.");
+
+startLoc = startLoc + new BlockOffset(0, -4);
+
+Console.WriteLine($"Now we are at: {startLoc.Row}, {startLoc.Column}");
+Console.WriteLine("Thanks for exploring Operand City!");
+
+public record BlockCoordinate(int Row, int Column)
+{
+    public static BlockCoordinate operator +(BlockCoordinate start, BlockOffset end) => new BlockCoordinate(start.Row + end.RowOffset, start.Column + end.ColumnOffset);
+    public static BlockCoordinate operator +(BlockOffset end, BlockCoordinate start) => start + end;
+
+    public static BlockCoordinate operator +(BlockCoordinate start, Direction direction) => direction switch
+    {
+        Direction.North => new BlockCoordinate(start.Row - 1, start.Column),
+        Direction.East => new BlockCoordinate(start.Row, start.Column + 1),
+        Direction.South => new BlockCoordinate(start.Row + 1, start.Column),
+        Direction.West => new BlockCoordinate(start.Row, start.Column - 1),
+        _ => throw new ArgumentOutOfRangeException(nameof(direction), direction, null)
+    };
+    public static BlockCoordinate operator +(Direction direction, BlockCoordinate start) => start + direction;
+}
+public record BlockOffset(int RowOffset, int ColumnOffset);
+public enum Direction { North, East, South, West }
